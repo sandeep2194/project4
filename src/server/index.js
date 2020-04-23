3534275b3a5af22dc90d4d04c4e9c2efa63ffe73
Sandeep Singh <sandeepsingh2194@gmail.com>
2020-04-23 21:39:10 +0530

    api setup

--- src/server/index.js
+++ src/server/index.js
@@ -2,12 +2,20 @@ var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
 var aylien = require("aylien_textapi");
+const dotenv = require('dotenv');
+dotenv.config();
+
+console.log(`Your API key is ${process.env.API_KEY}`);
 
 const app = express()
 
 app.use(express.static('dist'))
 
 console.log(__dirname)
+var textapi = new aylien({
+    application_id: process.env.API_ID,
+    application_key: process.env.API_KEY
+});
 
 app.get('/', function(req, res) {
     // res.sendFile('dist/index.html')
