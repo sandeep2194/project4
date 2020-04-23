ce7140657730e644d5cf504c23c97bda72be6a65
Sandeep Singh <sandeepsingh2194@gmail.com>
2020-04-23 20:39:32 +0530

    done setting up css loaders

--- webpack.dev.js
+++ webpack.dev.js
@@ -8,12 +8,19 @@ module.exports = {
     mode: 'development',
     devtool: 'source-map',
     stats: 'verbose',
+    output: {
+        libraryTarget: 'var',
+        library: 'Client'
+    },
     module: {
-        rules: [
-            {
+        rules: [{
                 test: '/\.js$/',
                 exclude: /node_modules/,
                 loader: "babel-loader"
+            },
+            {
+                test: /\.scss/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
             }
         ]
     },
