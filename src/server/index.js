43c332e9dfcf267a175131e6f40a749c5d795fe8
Sandeep Singh <sandeepsingh2194@gmail.com>
2020-04-23 20:45:18 +0530

    fixed broken scope issue
    using output in webpack config

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,7 @@
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
+var aylien = require("aylien_textapi");
 
 const app = express()
 
@@ -8,16 +9,16 @@ app.use(express.static('dist'))
 
 console.log(__dirname)
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
     // res.sendFile('dist/index.html')
     res.sendFile(path.resolve('src/client/views/index.html'))
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
+app.listen(8081, function() {
     console.log('Example app listening on port 8080!')
 })
 
-app.get('/test', function (req, res) {
+app.get('/test', function(req, res) {
     res.send(mockAPIResponse)
 })
